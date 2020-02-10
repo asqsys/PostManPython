920913f96f5de092a03e3ddceec3c5cf2047177d
asqsys <asqsys.testing@gmail.com>
2020-02-10 13:40:25 +0100

    Update app.py
    To modify your path for import

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from modules.comparison_xml.resources import comparison_xml
+#from modules.comparison_xml.resources import comparison_xml
+#or you have to place the comparison_xml.py inside the folders: modules/comparison_xml/resources
+import comparison_xml
 
 app = Flask(__name__)
 app.register_blueprint(comparison_xml, url_prefix="/xml")
